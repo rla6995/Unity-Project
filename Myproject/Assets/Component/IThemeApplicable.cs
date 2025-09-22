44e44f4c6f5407e9804f1c8e93e7d7bfdd75a402
kimtaejin <164276503+rla6995@users.noreply.github.com>
2025-09-22 15:11:32 +0900

    Remove unused folder

--- Myproject/Assets/Component/IThemeApplicable.cs
+++
@@ -1,6 +0,0 @@
-using UnityEngine;
-
-public interface IThemeApplicable
-{
-    void ApplyTheme(bool isNight);
-}
--- Myproject/Assets/Component/TimingJudgeZone.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-public class TimingJudgeZone : MonoBehaviour
-{
-    public Transform judgeCenter;       // 판정 기준 위치
-    public Collider2D wowCollider;      // Wow 판정용 콜라이더 (BoxCollider2D 권장)
-    public Collider2D niceCollider;     // Nice 판정용 콜라이더
-}
